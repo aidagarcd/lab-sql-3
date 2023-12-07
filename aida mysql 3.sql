9d1d62952db0986b2a4e8a6d60060ee77324d93b
aidagarcd <aidagarcd@gmail.com>
2023-12-07 20:38:39 +0100

    aidas tuesday 2lab

---
+++ aida mysql 3.sql
@@ -0,0 +1,45 @@
+-- How many distinct (different) actors' last names are there?
+-- In how many different languages where the films originally produced? (Use the column language_id from the film table)
+-- How many movies were released with "PG-13" rating?
+-- Get 10 the longest movies from 2006.
+-- How many days has been the company operating (check DATEDIFF() function)?
+-- Show rental info with additional columns month and weekday. Get 20.
+-- Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
+-- How many rentals were in the last month of activity?
+
+use sakila;
+select distinct actor_id from actor;
+select distinct language_id from film;
+select distinct film_id from film_category where category_id = 13
+
+use sakila;
+
+SELECT film_id, title, length
+FROM film
+WHERE release_year = 2006
+ORDER BY length DESC
+LIMIT 10;
+
+SELECT DATEDIFF(NOW(), 'fecha_inicio_operaciones') AS days_operating;
+
+SELECT *,
+       MONTH(rental_date) AS rental_month,
+       DAYNAME(rental_date) AS rental_weekday
+FROM rental
+LIMIT 20;
+
+
+SELECT *,
+       DAYNAME(rental_date) AS rental_weekday,
+       case
+		when DAYNAME(rental_date) in ('Saturday', 'Sunday') then 'weekend'
+		else 'weekday'
+	end as 'daytype'
+FROM rental
+LIMIT 20;
+
+select * from rental order by  return_date desc limit 1;
+
+select count(*) from rental where month(return_date) = 8 and year(return_date) = 2005;
+
+
